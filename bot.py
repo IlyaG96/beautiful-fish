1d6b2abcbf975bdebb4b0295c825c06f9e7a91a3
IlyaG96 <ilyag_96@icloud.com>
2022-03-13 23:33:52 +1100

    show fish description

--- bot.py
+++ bot.py
@@ -7,13 +7,30 @@ from enum import Enum
 from telegram.ext import Filters, Updater
 from telegram.ext import CallbackQueryHandler, CommandHandler, MessageHandler
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
-from elastic_api import get_client_token, fetch_products
+from elastic_api import get_client_token, fetch_products, get_product_info
+from textwrap import dedent
 
 
 class BotStates(Enum):
-    ASK_QUESTION = 1
-    CHECK_ANSWER = 2
+    START = 1
+    HANDLE_MENU = 2
     USER_CHOSE_ACTION = 3
+
+
+def format_product_description(product_description):
+    product_description = product_description.get('data')
+    print(product_description)
+
+    formatted_product_description = dedent(
+        f'''
+        Название : {product_description['name']}
+        Цена : {product_description['meta']['display_price']['with_tax']['formatted']} usd/кг
+        На складе : {product_description['meta']['stock']['level']} кг
+        Описание : {product_description['description']}
+        '''
+    )
+
+    return formatted_product_description
 
 
 def start(update, context):
@@ -21,15 +38,20 @@ def start(update, context):
     client_secret = context.bot_data['client_secret']
     elastic_token = get_client_token(client_secret, client_id)
     products = fetch_products(elastic_token)
+    context.bot_data['products'] = products
+    bot = context.bot
 
     keyboard = [[
         InlineKeyboardButton(product.get('name'), callback_data=product.get('id')) for product in products
     ]]
+
     reply_markup = InlineKeyboardMarkup(keyboard)
 
-    update.message.reply_text('Мы рыбов продоем!', reply_markup=reply_markup)
+    bot.send_message(text='Мы рыбов продоем!',
+                     chat_id=update.message.chat_id,
+                     reply_markup=reply_markup)
 
-    return BotStates.ASK_QUESTION
+    return BotStates.HANDLE_MENU
 
 
 def cancel(update, context):
@@ -42,12 +64,31 @@ def cancel(update, context):
     return ConversationHandler.END
 
 
-def button(update, context):
-    query = update.callback_query
+def handle_menu(update, context):
+
+    products = context.bot_data['products']
+    client_id = context.bot_data['client_id']
+    client_secret = context.bot_data['client_secret']
     bot = context.bot
-    bot.edit_message_text(text="Selected option: {}".format(query.data),
-                          chat_id=query.message.chat_id,
-                          message_id=query.message.message_id)
+
+    elastic_token = get_client_token(client_secret, client_id)
+
+    callback_query = update.callback_query
+    product_id = callback_query.data
+    product_description = get_product_info(elastic_token, product_id)
+    formatted_product_description = format_product_description(product_description)
+
+    keyboard = [[
+        InlineKeyboardButton(product.get('name'), callback_data=product.get('id')) for product in products
+    ]]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    bot.edit_message_text(text=formatted_product_description,
+                          chat_id=callback_query.message.chat_id,
+                          message_id=callback_query.message.message_id,
+                          reply_markup=reply_markup)
+
+    return BotStates.HANDLE_MENU
 
 
 def main():
@@ -78,8 +119,11 @@ def main():
             CommandHandler('cancel', cancel)
         ],
         states={
-            BotStates.ASK_QUESTION: [
-                CallbackQueryHandler(button),
+            BotStates.START: [
+                CallbackQueryHandler(start)
+            ],
+            BotStates.HANDLE_MENU: [
+                CallbackQueryHandler(handle_menu),
             ],
 
         },
